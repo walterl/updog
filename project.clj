741570791ab32148a8cc8ce21359c71b8c835906
Walter Leibbrandt <git@wrl.co.za>
2020-06-16 07:35:44 +0200

    Download and unzip updates
    Unzip is untested.

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[cheshire "5.10.0"]
+                 [clj-http "3.10.1"]
                  [com.taoensso/timbre "4.10.0"]
                  [integrant "0.8.0"]
                  [integrant/repl "0.3.1"]
+                 [me.raynes/fs "1.4.6"]
                  [medley "1.3.0"]
                  [org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot updog.core
--- src/updog/updater.clj
+++ src/updog/updater.clj
@@ -1,5 +1,10 @@
 (ns updog.updater
-  (:require [integrant.core :as ig]
+  (:require [clj-http.client :as http]
+            [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :as str]
+            [integrant.core :as ig]
+            [me.raynes.fs :as fs]
             [taoensso.timbre :as log]
             [updog.apps-db :as apps-db]
             [updog.app-source :as app-source]))
@@ -8,6 +13,33 @@
   "Is version `a` newer than `b`?"
   [a b]
   (pos? (compare a b)))
+
+(defn- download-file!
+  [url dest]
+  (with-open [out (io/output-stream dest)]
+    (io/copy (:body (http/get url {:as :stream}))
+             out)))
+
+(defn- exec!
+  [filename dir command]
+  ;; TODO Test this
+  (sh (-> command
+             (str/replace #"APPFILE" filename)
+             (str/split #"\s+"))
+      :dir dir))
+
+(defn- unzip
+  [zip-file parent-dir]
+  (sh ["unzip" zip-file] :dir parent-dir))
+
+(defn- post-process
+  [app-data]
+  ;; TODO Test this
+  (let [{:keys [local-path post-proc]} app-data
+        parent-dir                     (fs/parent local-path)]
+    (cond
+      (= :unzip post-proc) (unzip local-path parent-dir)
+      (string? post-proc)  (exec! local-path parent-dir post-proc))))
 
 (defprotocol Updater
   "File updater protocol."
@@ -23,17 +55,23 @@
     (doseq [[app-key app-data] (seq (apps-db/get-all-apps db))
             :let [{current-version :version
                    app-name        :name
-                   app-source      :source} app-data
-                  source                    (some #(when (= app-source (app-source/source-type %))
-                                                     %)
-                                                  sources)
-                  latest-version            (app-source/fetch-latest-version! source app-data)]]
-      (if (newer-version? (:version latest-version) current-version)
-        (log/infof "App %s/%s (%s) has newer version: %s"
-                   app-name
-                   app-key
-                   current-version
-                   latest-version)
+                   app-source      :source
+                   local-path      :local-path} app-data
+                  source         (some #(when (= app-source (app-source/source-type %))
+                                          %)
+                                       sources)
+                  {latest-version :version
+                   download-url   :download-url} (app-source/fetch-latest-version! source app-data)]]
+      (if (newer-version? latest-version current-version)
+        (do
+          (log/infof "App %s/%s (%s) has newer version: %s"
+                     app-name
+                     app-key
+                     current-version
+                     latest-version)
+          (download-file! download-url local-path)
+          (apps-db/assoc-field! db app-key :version latest-version)
+          (post-process app-data))
         (log/infof "App %s is at the latest version: %s"
                    app-name
                    current-version)))))
