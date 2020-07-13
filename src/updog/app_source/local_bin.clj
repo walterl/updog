6ce643e5f5cca79d61738de032c4c0594c1a1a06
Walter Leibbrandt <git@wrl.co.za>
2020-07-13 02:12:29 +0200

    Add local bin app source

---
+++ src/updog/app_source/local_bin.clj
@@ -0,0 +1,42 @@
+(ns updog.app-source.local-bin
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.string :as str]
+            [integrant.core :as ig]
+            [taoensso.timbre :as log]
+            [updog.app-source :refer [AppSource source-type]]
+            [updog.util :as u]))
+
+(defn- bin-version
+  "Get bin version from running `cmd --version`."
+  [cmd]
+  (-> (sh cmd "--version")
+      :out
+      str/trim
+      (str/split #"\s+")
+      last))
+
+(defrecord LocalBinSource []
+  AppSource
+  (source-type
+    [_]
+    :local-bin)
+
+  (fetch-latest-version!
+    [_ {:keys [local-bin], :as app}]
+    (log/debug ::fetch-latest-version! app)
+    {:version  (bin-version local-bin)
+     :filename local-bin
+     :location local-bin})
+
+  (download!
+    [_ {:keys [location]} dest-path]
+    (u/copy! location dest-path)))
+
+(defmethod u/->str LocalBinSource
+  [src]
+  (name (source-type src)))
+
+(defmethod ig/init-key ::app-source
+  [_ config]
+  (log/debug "Initializing app source: local bin")
+  (map->LocalBinSource config))
