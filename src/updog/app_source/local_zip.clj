6a519e7ba478aba251aa06672df0431a90f986b3
Walter Leibbrandt <git@wrl.co.za>
2020-08-10 04:28:23 +0200

    Add local zip app source

---
+++ src/updog/app_source/local_zip.clj
@@ -0,0 +1,39 @@
+(ns updog.app-source.local-zip
+  (:require [integrant.core :as ig]
+            [me.raynes.fs :as fs]
+            [taoensso.timbre :as log]
+            [updog.app-source :refer [AppSource source-type]]
+            [updog.util :as u]))
+
+(defrecord LocalZipSource []
+  AppSource
+  (source-type [_] :local-zip)
+
+  (fetch-latest-version-data!
+    [_ {:keys [local-zip tmp-dir] :as app}]
+    (log/debug ::fetch-latest-version! app)
+    (try
+      (let [bin-path (first (u/unzipped-files local-zip tmp-dir))]
+        (u/chmod+x bin-path)
+        {:version  (u/cmd-version bin-path)
+         :bin-path bin-path
+         :filename (last (fs/split local-zip))
+         :location local-zip})
+      (catch java.nio.file.NoSuchFileException _
+        (log/error "Local zip file not found:" local-zip)
+        {:error ::file-not-found})))
+
+  (download!
+    [_ {:keys [bin-path tmp-dir], {:keys [location]} :latest-version}]
+    (if (fs/exists? bin-path)
+      bin-path
+      (first (u/unzipped-files location tmp-dir)))))
+
+(defmethod u/->str LocalZipSource
+  [src]
+  (name (source-type src)))
+
+(defmethod ig/init-key ::app-source
+  [_ config]
+  (log/debug "Initializing app source: local zip")
+  (map->LocalZipSource config))
