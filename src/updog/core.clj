5e92353770000f27bc1c1abc43a6a117b47f08d6
Walter Leibbrandt <git@wrl.co.za>
2020-08-18 02:52:46 +0200

    Print CLI output

--- src/updog/core.clj
+++ src/updog/core.clj
@@ -1,7 +1,15 @@
 (ns updog.core
-  (:require [updog.cli :as cli])
+  (:require [clojure.pprint :refer [pprint]]
+            [updog.cli :as cli])
   (:gen-class))
+
+(defn- print-result
+  [{:keys [output], :as m}]
+  (cond
+    (string? output) (println output)
+    (some? output)   (pprint output)
+    :else            (pprint m)))
 
 (defn -main
   [& args]
-  (cli/main args))
+  (print-result (cli/main args)))
