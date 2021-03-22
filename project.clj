f8a135a9738601fb2c1bc417324f11300a8ae6ae
Walter Leibbrandt <git@wrl.co.za>
2021-03-22 04:32:08 +0200

    Bump version for develop

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject updog "0.2.2"
+(defproject updog "0.2.3-SNAPSHOT"
   :description "Your watchdog for updates of software without updates."
   :url "https://github.com/walterl/updog"
   :license {:name "GPLv3"
