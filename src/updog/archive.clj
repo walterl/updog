c45f25301224d9ba0e525a6d5d1496a96fdff514
Walter Leibbrandt <git@wrl.co.za>
2024-11-09 14:20:50 +0200

    Add archive/list-executables and archive/largest-file

--- src/updog/archive.clj
+++ src/updog/archive.clj
@@ -2,9 +2,13 @@
   "File archive handling"
   (:require
     [clojure.java.io :as io]
-    [updog.fs :as fs]) 
+    [updog.fs :as fs])
   (:import
     [org.apache.commons.compress.archivers.zip ZipFile]))
+
+(defn- unsupported-archive
+  [filename]
+  (ex-info "Unsupported archive" {:type ::unsupported-archive, :archive filename}))
 
 (defmulti extract
   "Extract `_archive-files` from archive `_filename` into `_dest-dir`.
@@ -14,18 +18,14 @@
     (fs/extension filename)))
 
 (defmethod extract :default
-  [filename archive-files dest-dir]
-  (throw (ex-info "Unsupported archive"
-                  {:type ::unsupported-archive
-                   :archive filename
-                   :archive-files archive-files
-                   :dest-dir dest-dir})))
+  [filename _archive-files _dest-dir]
+  (throw (unsupported-archive filename)))
 
 (defmethod extract ".zip"
   [filename archive-files dest-dir]
   (let [zf (ZipFile. filename)
-        archive-files (set archive-files)
-        entries (filter #(contains? archive-files (.getName %))
+        archive-files (set (or archive-files []))
+        entries (filter #(or (empty? archive-files) (contains? archive-files (.getName %)))
                         (enumeration-seq (.getEntries zf)))]
     (doall
       (for [entry entries
@@ -55,3 +55,59 @@
     #_(map #(.getName %) (java.util.Collection/list (.getEntries zf))))
   (Throwable->map *e)
   ,)
+
+(defmulti list-executables
+  "Returns a list of all executable files' names in the specified archive."
+  (fn [filename]
+    (fs/extension filename)))
+
+(defmethod list-executables :default
+  [filename]
+  (throw (unsupported-archive filename)))
+
+(defmethod list-executables ".zip"
+  [filename]
+  (for [zip-entry (enumeration-seq (.getEntries (ZipFile. filename)))
+        :let [perms (fs/unpack-perms (.getUnixMode zip-entry))]
+        :when (some #(pos? (bit-and % fs/PERM_X)) (vals perms))]
+    (.getName zip-entry)))
+
+(defmethod list-executables ".tar.bz2"
+  [filename]
+  ;; TODO
+  )
+
+(defmethod list-executables ".tar.gz"
+  [filename]
+  ;; TODO
+  )
+
+(defmulti largest-file
+  "Returns the name of the largest file in the specified archive."
+  (fn [filename]
+    (fs/extension filename)))
+
+(defmethod largest-file :default
+  [filename]
+  (throw (unsupported-archive filename)))
+
+(defmethod largest-file ".zip"
+  [filename]
+  (:name (reduce
+           (fn [largest curr]
+             (if (< (get largest :size 0) (.getSize curr))
+               {:name (.getName curr)
+                :size (.getSize curr)}
+               largest))
+           {}
+           (enumeration-seq (.getEntries (ZipFile. filename))))))
+
+(defmethod largest-file ".tar.bz2"
+  [filename]
+  ;; TODO
+  )
+
+(defmethod largest-file ".tar.gz"
+  [filename]
+  ;; TODO
+  )
